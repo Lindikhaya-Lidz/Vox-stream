cddf7fe4db6f69a6cbaf753bd800be088fa90ee3
lidz <lindikhaya.maqolwana@gmail.com>
2021-02-09 00:23:17 +0200

    added a menu bar with opacity paint mobile menu list

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,7 +1,17 @@
-let burger = document.querySelector('.burger');
+const burger = document.querySelector('.burger');
+const CloseMenu = document.querySelector('.close');
+const mobileMenu = document.querySelector('.mobile-menu-container');
+const carouselText = document.querySelector('.carousel-text');
+
+burger.addEventListener('click',()=> {
+    burger.style.display = 'none';
+    //carouselText.style.display = "none";
+    mobileMenu.style.display = "inline-block";
+
+});
 
 
-
-burger.addEventListener('click',()=>{
-    
+CloseMenu.addEventListener('click',()=>{
+    mobileMenu.style.display = 'none';
+    burger.style.display = 'block';
 })
