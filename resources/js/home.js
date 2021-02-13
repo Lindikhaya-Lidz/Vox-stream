4cc7ddfa59677ab15070399e318f415ab261eeb7
lidz <lindikhaya.maqolwana@gmail.com>
2021-02-14 00:28:58 +0200

    recreated the whole site

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,10 +1,21 @@
 const burger = document.querySelector('.burger');
 const CloseMenu = document.querySelector('.close');
-const mobileMenu = document.querySelector('.mobile-menu-container');
+const mobileMenu = document.querySelector('.mobile-menu');
 const carouselText = document.querySelector('.carousel-text');
+const dotMenuBar = document.querySelector('.dot-bar');
+
+dotMenuBar.addEventListener('click',()=> {
+    let contactsAndSignin = document.querySelector('.contacts-signin');
+    if(contactsAndSignin.style.display ==='none'){
+        contactsAndSignin.style.display = 'inline-block';
+    }else{
+        contactsAndSignin.style.display = 'none';
+    }
+    console.log('I have been click');
+})
 
 burger.addEventListener('click',()=> {
-    burger.style.display = 'none';
+    //burger.style.display = 'none';
     //carouselText.style.display = "none";
     mobileMenu.style.display = "inline-block";
 
